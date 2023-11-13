a621fc88add39b740dd6d420606d5861fa382d24
Erick <estoledo1@utpl.edu.ec>
2023-11-12 23:43:16 -0500

    problema2

---
+++ condicionales/problema2/src/problema2/Problema2.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author StarMedia
+ */
+public class Problema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String descripcion = "Este producto dispone de joyas de un bajo valor";
+        int cantidad;
+        double precioU = 12.5;
+
+        System.out.println("Ingrese la cantidad del producto");
+        cantidad = entrada.nextInt();
+
+        double costoP = (cantidad * precioU);
+
+        if (cantidad > 50) {
+            costoP = (costoP - (costoP * 0.15));
+
+        }
+
+        System.out.printf("*******PEDIDO*******\nDescripcion: %s\nCantidad: %d\n"
+                + "Precio Unitario: %.2f\nCosto: %.2f", descripcion, 
+                cantidad, precioU, costoP);
+    }
+}
